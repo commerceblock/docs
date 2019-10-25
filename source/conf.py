a50942c55c374d4f43fb28c82b7ab15a9b7a715f
Tom Trevethan <tptrevethan@googlemail.com>
2019-10-25 11:23:42 +0100

    added logo

--- source/conf.py
+++ source/conf.py
@@ -59,6 +59,13 @@ exclude_patterns = ['_build', 'build', 'Thumbs.db', '.DS_Store']
 #
 html_theme = 'sphinx_rtd_theme'
 
+html_theme_options = {
+    'logo_only': True
+}
+
+html_logo = "logo.png"
+html_show_sourcelink = True
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
